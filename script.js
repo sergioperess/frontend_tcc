28801addd4d222e9c40699537d5a7a9aeb41921d
sergioperess <sergio_kaun@hotmail.com>
2024-06-12 13:48:53 -0300

    Tela de cadastro

---
+++ script.js
@@ -0,0 +1,47 @@
+const cadastro = document.querySelector("form");
+const IfirstName = document.querySelector(".firstName");
+const IlastName = document.querySelector(".lastName");
+const Icpf = document.querySelector(".cpf");
+const Isenha = document.querySelector(".senha");
+const Iemail = document.querySelector(".email");
+const ImodeloCelular = document.querySelector(".modeloCelular");
+
+function cadastrar(){
+    fetch("http://localhost:8080/users",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                firstName: IfirstName.value,
+                lastName: IlastName.value,
+                cpf: Icpf.value,
+                senha: Isenha.value,
+                email: Iemail.value,
+                modeloCelular: ImodeloCelular.value
+            })            
+        }
+    )
+    .then(function (res) {console.log(res) })
+    .catch(function (res) {console.log(res) })
+}
+
+function limpar(){
+
+    IfirstName.value = "";
+    IlastName.value = "";
+    Icpf.value = "";
+    Isenha.value = "";
+    Iemail.value = "";
+    ImodeloCelular.value = "";
+}
+
+cadastro.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    cadastrar();
+
+    limpar();
+});
